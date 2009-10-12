3102e9be44f740e1612a5d5ecce755f7d1069623
Jianxin Zhong (azuwis) <azuwis@gmail.com>
2009-10-12 17:56:19 +0800

    extract js from base.js and clean a bit

--- js/core.js
+++ js/core.js
@@ -28,8 +28,16 @@
         jQuery.ajax({
             url: "/inc/feeds.html",
             success: function (html) {
-                jQuery('div#subscribe').append(html);
-                feeds_cb();
+                jQuery('div#subscribe').append(html)
+                .hover(function () {
+                    jQuery(this).find('ul').css({
+                        left: jQuery('#feedrss').cumulativeOffset(this)[0],
+                        top: jQuery('#feedrss').cumulativeOffset(this)[1]
+                    }).fadeIn(200)
+                },
+                function () {
+                    jQuery(this).find('ul').fadeOut(400)
+                });
             }
         });
     });
@@ -106,7 +114,26 @@
     });
 })();
 
-// base.js from inove
-var feeds_cb = function () {
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2.h.6=3(e){i t=0,l=0;j{t+=e.k||0;l+=e.m||0;e=e.n}o(e);7[l,t]};2(p).q(3(){2(\'a[r*="s"]\').8(3(){u.v(4.5);7 9});2(\'#w\').x(3(){2(4).b(\'c\').y({z:2(\'#d\').6(4)[0],A:2(\'#d\').6(4)[1]}).B(C)},3(){2(4).b(\'c\').D(f)});2(\'a[5!="#"][5!="#E"][5^="#"]\').8(3(){2.F(4.5.G(/^.*#/g,\'#\'),f,{H:0});7 9})});',44,44,'||jQuery|function|this|href|cumulativeOffset|return|click|false||find|ul|feedrss||400||fn|var|do|offsetTop||offsetLeft|offsetParent|while|document|ready|rel|external||window|open|subscribe|hover|css|left|top|fadeIn|200|fadeOut|ViewPollResults|scrollTo|replace|offset'.split('|'),0,{}))
-}
+// extract from base.js of inove theme
+jQuery.fn.cumulativeOffset = function (e) {
+    var t = 0,
+    l = 0;
+    do {
+        t += e.offsetTop || 0;
+        l += e.offsetLeft || 0;
+        e = e.offsetParent
+    } while (e);
+    return[l, t]
+};
+jQuery(document).ready(function () {
+    jQuery('a[rel*="external"]').click(function () {
+        window.open(this.href);
+        return false
+    });
+    jQuery('a[href!="#"][href!="#ViewPollResults"][href^="#"]').click(function () {
+        jQuery.scrollTo(this.href.replace(/^.*#/g, '#'), 400, {
+            offset: 0
+        });
+        return false
+    })
+});
