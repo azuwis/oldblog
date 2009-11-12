8f16dacbb350570b03f22703d38e4c685ffb62bf
Jianxin Zhong (azuwis) <azuwis@gmail.com>
2009-11-12 19:48:24 +0800

    move slide_post to core.js

--- js/core.js
+++ js/core.js
@@ -1,3 +1,64 @@
+// slide post
+var slide_post = function () {
+    var PRE_LOAD = 2;
+    jQuery('div.post').each(function () {
+        var id = jQuery(this).attr('id');
+        var pc = jQuery('#' + jq(id) + ' .content-wrap');
+        if (/^post\-/.test(id)) {
+            var toggle = jQuery('<a href="javascript:void(0);" class="toggle"></a>');
+            toggle.toggle(function () {
+    	    if (pc.find(".content").text() == '') {
+    		loadPost(id);
+    	    } else if (pc.find(".gcse").length > 0) {
+    		pc.hide();
+    		loadPost(id);
+                }
+    	    pc.fadeIn();
+    	    jQuery(this).removeClass('collapse').addClass('expand');
+            },
+            function () {
+                pc.slideUp();
+    	    jQuery(this).removeClass('expand').addClass('collapse');
+    	}).prependTo(jQuery('#' + jq(id) + ' h1'));
+        }
+    });
+    if (PRE_LOAD > 0) {
+        jQuery('div.post a.toggle').each(function (index) {
+            if (index < PRE_LOAD) {
+    	    if(jQuery(this).parent().parent().find(".gcse").length == 0) {
+    		jQuery(this).click();
+                }
+            }
+        });
+    }
+};
+function jq(myid) {
+    return myid.replace(/:/g,"\\:").replace(/\./g,"\\.").replace(/\//g,"\\/");
+};
+function loadPost(id) {
+    var postId = id.slice(5);
+    jQuery.ajax({
+        type: 'GET',
+        url: postId == "__" ? "/" : postId.replace(/__/g,"/") + '.html',
+        //cache: false,
+        dataType: 'html',
+        contentType: 'application/json; charset=utf-8',
+        beforeSend: function (data) {
+    	loadPostContent(id, '<p class="ajax-loader">Loading...</p>');
+        },
+        success: function (data) {
+    	data = jQuery(data).find('div.post div.content').html();
+    	loadPostContent(id, data);
+        },
+        error: function (data) {
+    	loadPostContent(id, '<p>Oops, It seems the post does not exist. Try the direct post link or <small><a href="javascript:void(0);" onclick="loadPost(\'' + id + '\');">[Reload]</a></small>.</p><p>If you are searching or click any tag link, please see the note below.</p>');
+        }
+    });
+    function loadPostContent(id, data) {
+        jQuery('#' + jq(id) + ' .content').html(data).truncate({max_length: 1200});
+    }
+};
+
 // google cse search box
 (function () {
     var searchbox = document.getElementById("searchbox");
@@ -43,9 +104,14 @@ var gcse = function(text, start){
     var div = jQuery("div#main");
     div.html('<p class="ajax-loader">Searching on Google Custom Search"' + text + '"...</p>');
     jQuery.getJSON(url, function (data) {
-        jQuery.get("/inc/gcse.html", function(tmpl){
-            div.pureJSTemplate({id:"gcse", data:{r:data.responseData.results, c:data.responseData.cursor, t:text}, tmpl:tmpl});
+        jQuery.ajax({
+            async: false,
+            url: "/inc/gcse.html",
+            success: function(tmpl){
+                div.pureJSTemplate({id:"gcse", data:{r:data.responseData.results, c:data.responseData.cursor, t:text}, tmpl:tmpl});
+            }
         });
+        slide_post();
     });
 };
 
@@ -153,4 +219,11 @@ jQuery(document).ready(function () {
         gcse(jQuery(this).text());
         return false;
     });
+    if (/^\/(index\.|$)/.test(window.location.pathname)) {
+        slide_post();
+    }
 });
+
+window['gcse'] = gcse;
+window['slide_post'] = slide_post;
+window['loadPost'] = loadPost;
